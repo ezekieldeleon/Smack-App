7405d95eee0d6cc6b03efa85464a3905857f1cdf
Ezekiel De Leon <ezekiel.d.e@ieee.org>
2018-06-19 06:52:44 -0700

    finished adding a getter for channels.

---
+++ Smack/Smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Ezekiel De Leon on 6/19/18.
+//  Copyright © 2018 Ezekiel De Leon. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
